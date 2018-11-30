a8d6e5818978e8d0e177661f6849c14dc2752bd4
Torin Kwok <torin@kwok.im>
2018-11-30 14:04:25 +0800

    Introduced table.save() and table.load() so as to serialize the blueprint table

--- rdaneel.lua
+++ rdaneel.lua
@@ -19,6 +19,135 @@ local rdaneel = {
 rdaneel._constants = { slotsNum = 16, }
 rdaneel._constants = rdaneel._pri.protect( rdaneel._constants )
 
+local function exportstring( s )
+    return string.format( "%q", s )
+end
+
+--[[ Save Table to File
+
+    Only Saves Tables, Numbers and Strings Insides Table
+    References are saved Does not save Userdata, Metatables,
+    Functions and indices of these.
+
+    ON FAILURE: Returns an error message.
+]]
+function table.save ( tbl, filename )
+
+    local charS, charE = "   ","\n"
+    local file, err = io.open( filename, "w" )
+
+    if err then return err end
+
+    -- initiate variables for save procedure
+    local tables, lookup = { tbl }, { [tbl] = 1 }
+    file:write( "return {"..charE )
+
+    for idx, t in ipairs( tables ) do
+        file:write( "-- Table: {"..idx.."}"..charE )
+        file:write( "{"..charE )
+
+        local thandled = {}
+
+        for i,v in ipairs( t ) do
+            thandled[i] = true
+
+            local stype = type( v )
+
+            -- only handle value
+            if stype == "table" then
+                if not lookup[v] then
+                    table.insert( tables, v )
+                    lookup[v] = #tables
+                end
+                file:write( charS.."{"..lookup[v].."},"..charE )
+
+            elseif stype == "string" then
+                file:write(  charS..exportstring( v )..","..charE )
+
+            elseif stype == "number" then
+                file:write(  charS..tostring( v )..","..charE )
+            end
+        end
+
+        for i,v in pairs( t ) do
+
+            -- escape handled values
+            if not thandled[i] then
+                local str = ""
+                local stype = type( i )
+
+                -- handle index
+                if stype == "table" then
+                    if not lookup[i] then
+                        table.insert( tables,i )
+                        lookup[i] = #tables
+                    end
+                    str = charS .. "[{"..lookup[i].."}]="
+
+                elseif stype == "string" then
+                    str = charS .. "["..exportstring( i ).."]="
+
+                elseif stype == "number" or stype == "boolean" then
+                    str = charS .. "["..tostring( i ).."]="
+                end
+                
+                if str ~= "" then
+                    stype = type( v )
+                    -- handle value
+                    if stype == "table" then
+                        if not lookup[v] then
+                            table.insert( tables,v )
+                            lookup[v] = #tables
+                        end
+                        file:write( str.."{"..lookup[v].."},"..charE )
+
+                    elseif stype == "string" then
+                        file:write( str..exportstring( v )..","..charE )
+
+                    elseif stype == "number" then
+                        file:write( str..tostring( v )..","..charE )
+                    end
+                end
+            end
+        end
+        file:write( "},"..charE )
+    end
+    file:write( "}" )
+    file:close()
+end
+
+--[[ Load Table from File
+    
+    Loads a table that has been saved via the table.save()
+    function.
+    
+    ON SUCCESS: Returns a previously saved table.
+    ON FAILURE: Returns as second argument an error msg.
+]]
+function table.load ( sfile )
+    local ftables,err = loadfile( sfile )
+
+    if err then return _, err end
+
+    local tables = ftables()
+    for idx = 1, #tables do
+        local tolinki = {}
+        for i, v in pairs( tables[idx] ) do
+            if type( v ) == "table" then
+                tables[idx][i] = tables[v[1]]
+            end
+            if type( i ) == "table" and tables[i[1]] then
+                table.insert( tolinki, { i,tables[i[1]] } )
+            end
+        end
+        -- link indices
+        for _, v in ipairs( tolinki ) do
+            tables[idx][v[2]],tables[idx][v[1]] =  tables[idx][v[1]],nil
+        end
+    end
+    return tables[1]
+end
+
 function turtle:goup ( destroy )
     if self and turtle.detectUp() then
         turtle.digUp()
@@ -385,8 +514,8 @@ logFormat = "Round=%d; Dir=%s; nthStep=%d; [X=%d Y=%d Z=%d]; DONE=%s"
 
 local tree = {}
 success, err = rdaneel:sweepSolid {
-    length = 4, width = 3, height = 1,
-    reversed = false,
+    length = 4, width = 3, height = 3,
+    reversed = true,
     sweepCallback = function ( info )
         local exists, details = turtle.inspectDown()
         if not exists then details = {} end
@@ -418,8 +547,15 @@ success, err = rdaneel:sweepSolid {
                       { x = info.x, y = info.y, block = details, } )
     end
 }
+
 assert( success, err )
+assert( table.save( tree, "blueprint.dat" ) == nil )
+
+local blueprintTbl, err = table.load( "blueprint.dat" )
+assert( err == nil )
 
 logFH.writeLine( rdaneel:tprint( tree, 2, result ) )
 logFH.writeLine( '*' )
+logFH.writeLine( rdaneel:tprint( blueprintTbl, 2, result ) )
+
 logFH.close()
